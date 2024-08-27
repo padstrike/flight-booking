3f0af7cb323a7542164d1316fc64c9b02985b6c1
padstrike <poramet1797@gmail.com>
2024-08-27 11:01:02 +0700

    Initial commit

--- backend/routes/flight.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { allFlights,searchFlights } = require('../controllers/flightController');
-
-// Search flights
-router.get('/', allFlights);
-router.post('/search', searchFlights);
-
-module.exports = router;
--- backend/routes/payment.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createPayment } = require('../controllers/paymentController');
-const auth = require('../middleware/auth');
-
-// Route to process payment and create a booking
-router.post('/create', auth, createPayment);
-
-module.exports = router;
